8d9c6361b7e57245e2a18dd5cf28791e09465401
danremel <remel.dan@gmail.com>
2017-07-19 19:28:14 -0400

    Created first greeting message

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -2,7 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+	const greeting = "Welcome to Pizza Express!";
+	res.send(greeting);
+});
+
+
 
 app.listen(port, () => {
 	console.log('Listening on port ' + port);
