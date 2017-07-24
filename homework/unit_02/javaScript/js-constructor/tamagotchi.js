dbceb9ba9e776dc12379acf92a7f53db8ebb4f27
danremel <remel.dan@gmail.com>
2017-07-24 19:50:58 -0400

    Part 2 and 3: added arguments and new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -12,10 +12,21 @@ const Tamagotchi = function(name, creatureType) {
 		console.log("WAHHHH!!!");
 		console.log(this.name + " has this much food in its tummy: " + this.foodInTummy);
 	};
+	this.puke = function () {
+		this.foodInTummy--;
+		console.log("Hurrrllll!");
+		console.log(this.name + "'s food level is now: " + this.foodInTummy);
+	};
+	this.yawn = function () {
+		this.restedness--;
+		console.log("Yawnnnnn! I'm sleepy.");
+		console.log(this.name + "'s restedness level is now: " + this.restedness);
+	};
 }
 
 //create new Tamagotchis
-var gizmo = new Tamagotchi("Gizmo", "Darno");
+var gizmo = new Tamagotchi("Gizmo", "Mametchi");
 
 //test out your Tamagotchies below via console.logs
-console.log(gizmo.cry());
+console.log(gizmo.yawn());
+console.log(gizmo.puke());
