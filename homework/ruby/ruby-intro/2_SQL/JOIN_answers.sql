2a6c74c32489f960e0e7616c846248b5454fdba2
danremel <remel.dan@gmail.com>
2017-08-18 22:35:32 -0400

    Completed JOIN exercises with all correct answers

---
+++ homework/ruby/ruby-intro/2_SQL/JOIN_answers.sql
@@ -0,0 +1,50 @@
+1.  SELECT matchid, player FROM goal 
+     WHERE teamid = 'GER';
+2.  SELECT id,stadium,team1,team2
+  FROM game WHERE id = 1012;
+3.  SELECT player, teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+  WHERE teamid = 'GER';
+4. SELECT team1, team2, player
+FROM game JOIN goal ON game.id = goal.matchid
+WHERE player LIKE 'Mario%';
+5. SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON teamid = id
+ WHERE gtime<=10;
+6.  SELECT game.mdate, eteam.teamname
+ FROM game JOIN eteam ON game.team1 = eteam.id
+ WHERE eteam.coach = 'Fernando Santos';
+7.  SELECT goal.player
+ FROM goal JOIN game ON game.id = goal.matchid
+ WHERE UPPER(game.stadium) = UPPER('National Stadium, Warsaw');
+8.  SELECT DISTINCT player
+FROM game JOIN goal ON goal.matchid = game.id
+WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid <> 'GER';
+9.  SELECT teamname, COUNT(gtime)
+  FROM eteam JOIN goal ON id=teamid
+ GROUP BY teamname;
+10. SELECT DISTINCT game.stadium, COUNT(goal.gtime)
+ FROM game JOIN goal ON game.id = goal.matchid
+ GROUP BY stadium
+11. SELECT game.id, game.mdate, COUNT(*)
+FROM game JOIN goal ON game.id = goal.matchid
+WHERE (game.team1 = 'POL' OR game.team2 = 'POL')
+GROUP BY game.id, game.mdate ORDER BY game.id;
+12. SELECT game.id, game.mdate, COUNT(*)
+FROM game JOIN goal ON goal.matchid = game.id
+WHERE goal.teamid = 'GER'
+GROUP BY game.id, game.mdate; 
+13. SELECT game.mdate, 
+       game.team1, 
+       SUM(CASE WHEN goal.teamid = game.team1
+           THEN 1
+           ELSE 0
+           END) AS score1,
+       game.team2,
+       SUM(CASE WHEN goal.teamid = game.team2
+           THEN 1
+           ELSE 0
+           END) AS score2
+FROM game JOIN goal ON (game.id = goal.matchid)
+GROUP BY game.id
+ORDER BY game.mdate, goal.matchid;
