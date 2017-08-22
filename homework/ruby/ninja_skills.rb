7c04825a0d27b43a6bcac4aebbb9f71e760c0f26
danremel <remel.dan@gmail.com>
2017-08-21 21:58:50 -0400

    4. String Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -10,9 +10,19 @@
 
 
 ## Integers
-d = 1
-puts d
-e = 2.3
-puts e
-d = e
-puts d
+# d = 1
+# puts d
+# e = 2.3
+# puts e
+# d = e
+# puts d
+
+
+## Strings
+christine = "Hi, I'm Christine"
+likes_to = "long wants on the beach, kinda."
+sentence = "#{christine} and I like #{likes_to}"
+puts sentence
+g = "2"
+g_to_number = 2
+puts g.to_i + g_to_number
