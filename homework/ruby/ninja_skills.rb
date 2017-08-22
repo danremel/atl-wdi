f3563a35aff6dfa582363c7dad9d89c785294bc6
danremel <remel.dan@gmail.com>
2017-08-21 22:03:49 -0400

    5. Input/Output Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -19,10 +19,19 @@
 
 
 ## Strings
-christine = "Hi, I'm Christine"
-likes_to = "long wants on the beach, kinda."
-sentence = "#{christine} and I like #{likes_to}"
-puts sentence
-g = "2"
-g_to_number = 2
-puts g.to_i + g_to_number
+# christine = "Hi, I'm Christine"
+# likes_to = "long wants on the beach, kinda."
+# sentence = "#{christine} and I like #{likes_to}"
+# puts sentence
+# g = "2"
+# g_to_number = 2
+# puts g.to_i + g_to_number
+
+
+## Input and Output
+# puts "What is your name?"
+# name = gets.chomp
+# puts "What do you like to do?"
+# activity = gets.chomp.downcase
+# puts "I am #{name} and I like to go #{activity}"
+
