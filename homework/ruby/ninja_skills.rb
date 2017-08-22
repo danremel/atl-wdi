0b8b51cf72521df6f0a863a56233e816c53847f1
danremel <remel.dan@gmail.com>
2017-08-21 21:54:56 -0400

    3. Integers Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,6 +1,18 @@
-# Booleans
-a = true
-b = false
-puts a == b
-puts a || b
+## Booleans
+# a = true
+# b = false
+# puts a == b
+# puts a || b
 
+## Nil
+# n = nil
+# p n
+
+
+## Integers
+d = 1
+puts d
+e = 2.3
+puts e
+d = e
+puts d
